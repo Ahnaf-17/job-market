c826a0c7305654bc457cf53daa5234ea27022b08
unknown <ahnaf.ahmed.42173@gmail.com>
2023-09-23 01:05:58 +0600

    added

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Ayhay page to nai !!!</h1>
+            <Link to='/'>Go back</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
